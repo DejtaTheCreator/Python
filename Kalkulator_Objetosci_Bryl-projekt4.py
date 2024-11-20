8ce870532b82f9864995e1dbd6b6bbe811f1e485
DejtaTheCreator <dataoliwier@gmail.com>
2024-11-20 22:18:18 +0000

    SKONCZONE , DZIALA

--- Kalkulator_Objetosci_Bryl-projekt4.py
+++ Kalkulator_Objetosci_Bryl-projekt4.py
@@ -1,3 +1,4 @@
+import math
 
 print("Witaj w programie obliczajacym obejtosci bryl")
 
@@ -59,62 +60,59 @@ def ostroslup2():#trojkat w podstawie
         return("Zadna z dlugosci nie moze byc mniejsza lub rowna 0")
 
 def kula():
-   pi = 3,14
    r = int(input("Podaj promien kuli :")) 
    if r>0:
-        return 4/3*pi*r**3
+        return (4/3*math.pi*r**3)
    else:
        return("Promien nie moze byc ujemny")
    
 def stozek():
-    pi = 3,14
     r = int(input("Wprowadz promien podstawy: "))
     H = int(input("Wprowadz wysokosc stozka: ")) 
     if r>0 and H>0:
-        return pi*r**2*H/3
+        return (math.pi*r**2*H/3)
     else:
         return("Zadna z dlugosci nie moze byc mniejsza lub rowna 0")
 
 def walec():
-    pi = 3,14
     r = int(input("Podaj promien podstawy: "))
     H = int(input("Podaj wysokosc walca: "))
     if r>0 and H>0:
-        return pi*r**2*H
+        return (math.pi*r**2*H)
     else:
         return("Zadna z dlugosci nie moze byc mniejsza lub rowna 0")
     
 
-print("1. Objetosc Szescianu")
-print("2. Objetosc prostopadloscianu")
-print("3. Objetosc graniastoslupa o podstawie trojkata")
-print("4. Objetosc graniastoslupa o podstawie trapezu")
-print("5. Objetosc ostroslupa o podstawie prostokata")
-print("6. Objetosc ostroslupa o podstawie trojkata")
-print("7. Objetosc kuli")
-print("8. Objetosc stozka")
-print("9. Objetosc walca")
+print("1. Objetosc_Szescianu")
+print("2. Objetosc_prostopadloscianu")
+print("3. Objetosc_graniastoslupa_o_podstawie_trojkata")
+print("4. Objetosc_graniastoslupa_o_podstawie_trapezu")
+print("5. Objetosc_ostroslupa_o_podstawie_prostokata")
+print("6. Objetosc_ostroslupa_o_podstawie_trojkata")
+print("7. Objetosc_kuli")
+print("8. Objetosc_stozka")
+print("9. Objetosc_walca")
 
 wybor = input("Wybierz opcję (1-9): ")
  
 if wybor == "1":
-    print(f"Objetosc Szescianu: {Objetosc_Szescianu():.2f}")
+    print(f"Objetosc Szescianu: {szescian():.2f}")
 elif wybor == "2":
-    print(f"Objetosc prostopadloscianu: {Objetosc_prostpadloscianiu():.2f}")
+    print(f"Objetosc prostopadloscianu: {prostopadloscian():.2f}")
 elif wybor == "3":
-    print(f"Objetosc graniastoslupa o podstawie trojkata: {Objetosc_graniastoslupa_o_podstawie_trojkata():.2f}")
+    print(f"Objetosc graniastoslupa o podstawie trojkata: {graniastoslup():.2f}")
 elif wybor == "4":
-    print(f"Objetosc graniastoslupa o podstawie trapezu: {Objetosc_graniastolupa_o_podstawie_trapezu():.2f}")
+    print(f"Objetosc graniastoslupa o podstawie trapezu: {graniastoslup2():.2f}")
 elif wybor == "5":
-    print(f"Objetosc ostroslupa o podstawie prostokata: {Objetosc_ostroslupa_o_podstawie_prostokata():.2f}")
+    print(f"Objetosc ostroslupa o podstawie prostokata: {ostroslup():.2f}")
 elif wybor == "6":
-    print(f"Objetosc ostroslupa o podstawie trojkata: {Objetosc_ostroslupa_o_podstawie_trojkata():.2f}")
+    print(f"Objetosc ostroslupa o podstawie trojkata: {ostroslup2():.2f}")
 elif wybor == "7":
-    print(f"Objetosc kuli: {Objetosc_kuli():.2f}")
+    print(f"Objetosc kuli: {kula():.2f}")
 elif wybor == "8":
-    print(f"Objetosc stozka: {Objetosc_stozka():.2f}")
+    print(f"Objetosc stozka: {stozek():.2f}")
 elif wybor == "9":
-    print(f"Objetosc walca: {Objetosc_walca():.2f}")
+    print(f"Objetosc walca: {walec():.2f}")
     
 else:
     print("Nieprawidłowy wybór. Spróbuj ponownie.")
